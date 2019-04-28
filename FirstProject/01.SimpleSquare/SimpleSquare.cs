688f6d817bfcf70a5915f3374bc5aaeca402cfb5
Sreedevi <sreedevivp4u@gmail.com>
2019-04-28 13:27:20 +0100

    initial commit

---
+++ FirstProject/01.SimpleSquare/SimpleSquare.cs
@@ -0,0 +1,24 @@
+﻿using SmallBasicFun;
+
+namespace SimpleSquare
+{
+	public class SimpleSquare
+	{
+		public static void Main()
+		{
+            Tortoise.Show();
+          
+            Tortoise.SetSpeed(10);
+         
+            for (int i = 0; i < 4; i++)
+            {
+                Tortoise.SetPenColor("blue");
+                Tortoise.Move(50);
+                Tortoise.Turn(90);
+
+            }
+          
+
+        }
+	}
+}
