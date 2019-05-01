c980b243f7bf004013c95e03e2337ff3f25b8535
Sreedevi <sreedevivp4u@gmail.com>
2019-05-01 22:25:31 +0100

    message box

--- FirstProject/01.SimpleSquare/SimpleSquare.cs
+++ FirstProject/01.SimpleSquare/SimpleSquare.cs
@@ -4,18 +4,15 @@ namespace SimpleSquare
 {
     public class SimpleSquare
 	{
-        private const int sides = 7;
-           
-
+    
         public static void Main()
 		{
             Tortoise.Show();
-          
-            Tortoise.SetSpeed(10);
-         
+            Tortoise.SetSpeed(5);
+            int sides = MessageBox.AskForInput("How many sides");
             for (int i = 0; i < sides; i++)
             {
-                Tortoise.SetPenWidth(2);
+                Tortoise.SetPenWidth(5);
                 Tortoise.SetPenColor(Colors.GetRandomColor());
                 Tortoise.Move(50);
                 Tortoise.Turn(360.0 / sides);
